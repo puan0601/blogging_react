6623203759d2c220d45265bd68b7c9ea1ac1bfff
puan0601 <anton.pugach@gmail.com>
2018-08-01 19:56:53 -0700

    creates FETCH_POSTS action

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+install promise from 'redux-promise';
 
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
